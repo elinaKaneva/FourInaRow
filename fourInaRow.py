5c3275e6f83807e884b3aacb66fd4ab58c568f86
Elina Kaneva <elina.kaneva@gmail.com>
2013-08-29 18:11:20 +0300

    board data - start collecting
    background changes in order to be able to collect data later

--- fourInaRow.py
+++ fourInaRow.py
@@ -15,11 +15,12 @@ RED = (214,71,0)
 BLUE = (0,102,153)
 DARK_BLUE = (0,49,101)
 LIGHT_BLUE = (185,231,255)
+NICE_BLUE = (202,202,255)
 
 # Coordinates:
 BOARD = (190, 240)
-COLUMNS = [(190, 180), (250, 180), (310, 180), (370, 180), (430, 180), (490, 180), (550, 180)]
-ROWS = [(0, 0), (0, 0), (0, 0), (0, 0), (0, 0), (0, 0)]
+COLUMNS = [190, 250, 310, 370, 430, 490, 550]
+ROWS = [0, 0, 0, 0, 0, 0]
 
 # Sizes:
 STEP = 60
@@ -27,52 +28,75 @@ STEP = 60
 class Starter(PygameHelper):
     def __init__(self):
         self.w, self.h = 800, 600
-        self.above_the_board = 0
-        self.column = -1
-        self.turn = 0
-        self.pulls = [[[0,0]]*len(COLUMNS)]*len(ROWS)
+        self.over_the_board = 0
+        self.head_column = -1
+        self.col = -1
+        self.row = -1
+        self.turn = 9
+        self.move = 0
+        self.free_space = 0
+        self.pulls = [[0]*len(COLUMNS)]*len(ROWS)
 
         self.Surface = pygame.display.set_mode((self.w, self.h))
 
         PygameHelper.__init__(self, size=(self.w, self.h), fill=(LIGHT_BLUE))
         self.background = pygame.image.load(os.path.join("pics", "background.png"))
-        self.cells = pygame.image.load(os.path.join("pics", "cells.png"))
+        self.below_board = pygame.image.load(os.path.join("pics", "below_board.png"))
+        self.board = pygame.image.load(os.path.join("pics", "board.png"))
         self.red_pull = pygame.image.load(os.path.join("pics", "red_pull.png"))
         self.yellow_pull = pygame.image.load(os.path.join("pics", "yellow_pull.png"))
         
     def update(self):
-        pass
-        
+        if self.move == 1 and self.free_space == 1:
+            self.pulls[self.row][self.col] = self.turn
+            print(self.pulls[self.row])
+            self.move = 0
+            print(self.pulls)
+
     def keyUp(self, key):
         pass
         
     def mouseUp(self, button, pos):
         x, y = pos[0], pos[1]
         
-        for col in COLUMNS:
-            if col[0] < x < col[0] + STEP:
-                self.column = COLUMNS.index(col)
-        else:
-            self.above_the_board = 0
-        
-        
+        for column in COLUMNS:
+            if column < x < column + STEP and button == 1:
+                self.col = COLUMNS.index(column)
+                print("selected col index:", self.col)
+                self.move = 1
+
+        for r in self.pulls:
+            if r[self.col] > 0:
+                if self.pulls.index(r) != 0:
+                    self.row = self.pulls.index(r) - 1
+                    self.free_space = 1
+
+                else:
+                    self.row = 5
+            print("selected row index:", self.row)
+
+
     def mouseMotion(self, buttons, pos, rel):
         x, y = pos[0], pos[1]
         
-        if x in range(BOARD[0],BOARD[0] + len(COLUMNS) * STEP) and y in range(0,BOARD[1] + 1):
-            self.above_the_board = 1
-            for col in COLUMNS:
-                if col[0] < x < col[0] + STEP:
-                    self.column = COLUMNS.index(col)
+        if x in range(BOARD[0],BOARD[0] + len(COLUMNS) * STEP):
+            self.over_the_board = 1
+            for column in COLUMNS:
+                if column < x < column + STEP:
+                    self.head_column = COLUMNS.index(column)
         else:
-            self.above_the_board = 0
-        
+            self.over_the_board = 0
+
+
     def draw(self):
-        self.screen.fill(color=LIGHT_BLUE)
-        if self.above_the_board == 1:
-            self.screen.blit(self.red_pull, dest=COLUMNS[self.column])
-            #self.screen.blit(self.yellow_pull, dest=COLUMNS[self.column])
-        self.screen.blit(self.cells, dest=BOARD)
+        self.screen.fill(color=NICE_BLUE)
+        self.screen.blit(self.below_board, dest=BOARD)
+        if self.over_the_board == 1:
+            if self.move == 1:
+                self.screen.blit(self.red_pull, dest=(BOARD[0] + self.col * STEP, BOARD[1] - STEP))
+            else:
+                self.screen.blit(self.red_pull, dest=(BOARD[0] + self.head_column * STEP, BOARD[1] - STEP))
+        self.screen.blit(self.board, dest=BOARD)
         
 s = Starter()
 s.mainLoop(FPS)
